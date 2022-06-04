b21b3ec1a1be0b9cf0050e2fbe22430bbf9818e1
kejoth <jruizoc02@gmail.com>
2022-06-04 14:35:58 -0500

    Added function playRound. This function lets player select from rock, paper or scissors, then runds computerPlay and then runs another function to retunr a result.

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
-let computerSelection = computerPlay();
+
 const hand = ["rock", "paper", "scissors"];
 function computerPlay(){
     return hand[Math.floor(Math.random() * hand.length)];
 }
+
+function playRound (){
+    let playerSelection= prompt("Rock, Paper, Scissors!", "choose").toLowerCase();
+    let computerSelection = computerPlay();
+    console.log(computerSelection);
+    
+
+    function play(){
+    if ((playerSelection==="rock")&&(computerSelection==="scissors")) {
+     return "You win! Rock beats Scissors";    
+    }
+    else if ((playerSelection==="paper")&&(computerSelection==="rock")){
+     return "You win! Paper beats Rock";    
+    }
+    else if ((playerSelection==="scissors")&&(computerSelection==="paper")){
+     return "You win! Scissors beats Paper";    
+    }
+    else if ((playerSelection==="rock")&&(computerSelection==="paper")){
+     return "You lose! Paper beats Rock";    
+    }
+    else if ((playerSelection==="paper")&&(computerSelection==="scissors")){
+     return "You lose! Scissors beats Paper";    
+    }
+    else if ((playerSelection==="scissors")&&(computerSelection==="rock")){
+     return "You lose! Rock beats Scissors";    
+    }
+    else if (playerSelection===computerSelection){
+     return "Draw!"
+    }
+    else{
+        return "Error!"
+    }
+    }
+    const result = play();
+
+    return result;
+    
+
+ }
+ console.log (playRound());
