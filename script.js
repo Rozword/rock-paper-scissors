7238003dc21fc286b7ba6e22aeb10d1e9a2d635a
kejoth <jruizoc02@gmail.com>
2022-06-04 16:19:09 -0500

    Added playerName variable. Changed alert message for it to display player's name instead of just 'player'

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function computerPlay(){
     return hand[Math.floor(Math.random() * hand.length)];
 }
 
+let playerName= prompt("Enter your name", "");
 let playerScore = (0);
 let computerScore = (0);
 let draw = (0);
@@ -17,25 +18,25 @@ function playRound (){
 
     function play(){
     if ((playerSelection==="rock")&&(computerSelection==="scissors")) {
-     alert ("You win this round! Rock beats Scissors\nScore: Player " + (++playerScore) + " Computer " + (computerScore) + " Draw: " + (draw));   
+     alert ("You win this round! Rock beats Scissors\nScore: " + (playerName) + ": " + (++playerScore) + " - Computer: " + (computerScore) + " - Draw: " + (draw));   
     }
     else if ((playerSelection==="paper")&&(computerSelection==="rock")){
-        alert ( "You win this round! Paper beats Rock\nScore: Player " + (++playerScore) + " Computer " + (computerScore) + " Draw: " + (draw));    
+        alert ( "You win this round! Paper beats Rock\nScore: " + (playerName) + ": " + (++playerScore) + " - Computer: " + (computerScore) + " - Draw: " + (draw));    
     }
     else if ((playerSelection==="scissors")&&(computerSelection==="paper")){
-        alert ( "You win this round! Scissors beats Paper\nScore: Player " + (++playerScore) + " Computer " + (computerScore) + " Draw: " + (draw));    
+        alert ( "You win this round! Scissors beats Paper\nScore: " + (playerName)+ ": " + (++playerScore) + " - Computer: " + (computerScore) + " - Draw: " + (draw));    
     }
     else if ((playerSelection==="rock")&&(computerSelection==="paper")){
-        alert ( "You lose this round! Paper beats Rock\nScore: Player " + (playerScore) + " Computer " + (++computerScore) + " Draw: " + (draw));    
+        alert ( "You lose this round! Paper beats Rock\nScore: " + (playerName)+ ": " + (playerScore) + " - Computer: " + (++computerScore) + " - Draw: " + (draw));    
     }
     else if ((playerSelection==="paper")&&(computerSelection==="scissors")){
-        alert ( "You lose this round! Scissors beats Paper\nScore: Player " + (playerScore) + " Computer " + (++computerScore) + " Draw: " + (draw));   
+        alert ( "You lose this round! Scissors beats Paper\nScore: " + (playerName)+ ": " + (playerScore) + " - Computer: " + (++computerScore) + " - Draw: " + (draw));   
     }
     else if ((playerSelection==="scissors")&&(computerSelection==="rock")){
-     alert ( "You lose this round! Rock beats Scissors\nScore: Player " + (playerScore) + " Computer " + (++computerScore) + " Draw: " + (draw));  
+     alert ( "You lose this round! Rock beats Scissors\nScore: " + (playerName)+ ": " + (playerScore) + " - Computer: " + (++computerScore) + " - Draw: " + (draw));  
     }
     else if (playerSelection===computerSelection){
-        alert ( "Draw!\nScore: Player " + (playerScore) + " Computer " + (computerScore) + " Draw: " + (++draw));  
+        alert ( "Draw!\nScore: " + (playerName)+ ": " + (playerScore) + " - Computer: " + (computerScore) + " - Draw: " + (++draw));  
     }
     else{
         alert ("Error!")
@@ -58,7 +59,7 @@ for (let i = 0; i < 5; i++) {
    } 
 
 if (parseInt(playerScore) > parseInt(computerScore)){
-    alert("Player is the winner of the game!");                
+    alert(playerName + " is the winner of the game!");                
 }
 else if (parseInt(playerScore) < parseInt(computerScore)){
     alert ("Computer is the winner of the game!");
