93ef5fc6df25bb60f6efbc444f819cf8dfd0d3e9
kejoth <jruizoc02@gmail.com>
2022-06-04 14:53:49 -0500

    Added two variables: playerScore & computerScore. expanded the return given by the function play(), inside function playRound(), for it to give a score as well.

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ function computerPlay(){
     return hand[Math.floor(Math.random() * hand.length)];
 }
 
+let playerScore = (0);
+let computerScore = (0);
+
+
 function playRound (){
     let playerSelection= prompt("Rock, Paper, Scissors!", "choose").toLowerCase();
     let computerSelection = computerPlay();
@@ -12,22 +16,22 @@ function playRound (){
 
     function play(){
     if ((playerSelection==="rock")&&(computerSelection==="scissors")) {
-     return "You win! Rock beats Scissors";    
+     return "You win! Rock beats Scissors\nScore: Player " + ((playerScore)+1) + " Computer " + (computerScore);    
     }
     else if ((playerSelection==="paper")&&(computerSelection==="rock")){
-     return "You win! Paper beats Rock";    
+     return "You win! Paper beats Rock\nScore: Player " + ((playerScore)+1) + " Computer " + (computerScore);;    
     }
     else if ((playerSelection==="scissors")&&(computerSelection==="paper")){
-     return "You win! Scissors beats Paper";    
+     return "You win! Scissors beats Paper\nScore: Player " + ((playerScore)+1) + " Computer " + (computerScore);    
     }
     else if ((playerSelection==="rock")&&(computerSelection==="paper")){
-     return "You lose! Paper beats Rock";    
+     return "You lose! Paper beats Rock\nScore: Player " + (playerScore) + " Computer " + ((computerScore)+1);    
     }
     else if ((playerSelection==="paper")&&(computerSelection==="scissors")){
-     return "You lose! Scissors beats Paper";    
+     return "You lose! Scissors beats Paper\nScore: Player " + (playerScore) + " Computer " + ((computerScore)+1);    
     }
     else if ((playerSelection==="scissors")&&(computerSelection==="rock")){
-     return "You lose! Rock beats Scissors";    
+     return "You lose! Rock beats Scissors\nScore: Player " + (playerScore) + " Computer " + ((computerScore)+1);    
     }
     else if (playerSelection===computerSelection){
      return "Draw!"
@@ -38,8 +42,10 @@ function playRound (){
     }
     const result = play();
 
-    return result;
-    
-
+    return result;  
  }
+ 
+
+
  console.log (playRound());
+ 
