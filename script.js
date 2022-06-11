74dd45db069b70f825584159336a62e8742130b0
kejoth <jruizoc02@gmail.com>
2022-06-10 21:45:00 -0500

    added simple ui to the rock, paper, scissors game. Most of the UI was added using DOM methods for the sake of practice.

--- script.js
+++ script.js
@@ -1,67 +1,244 @@
-
 const hand = ["rock", "paper", "scissors"];
 function computerPlay(){
     return hand[Math.floor(Math.random() * hand.length)];
 }
 
-let playerName= prompt("Enter your name", "");
+let playerName= prompt("Let's play 5 rounds of Rock, Paper, Scissors!", "Enter your name");
 let playerScore = (0);
 let computerScore = (0);
 let draw = (0);
 
+let playerSelection;
+let result;
+
 function playRound (){
    
-    let playerSelection;
-    do {
-        playerSelection= prompt("Rock, Paper, Scissors!", "choose").toLowerCase();
-    }     
-    while (playerSelection !=="rock" && playerSelection !=="paper" && playerSelection !=="scissors")    
+    //let playerSelection;
+    //do {
+    //    playerSelection= prompt("Rock, Paper, Scissors!", "choose").toLowerCase();
+    //}     
+    //while (playerSelection !=="rock" && playerSelection !=="paper" && playerSelection !=="scissors")    
         
     let computerSelection = computerPlay();
     console.log(computerSelection);
     
-    function play(){
+    
     if ((playerSelection==="rock")&&(computerSelection==="scissors")) {
-     alert ("You win this round! Rock beats Scissors\nScore: " + (playerName) + ": " + (++playerScore) + " - Computer: " + (computerScore) + " - Draw: " + (draw));   
+        return result = ("Your opponent played " + (computerSelection) + "\n\nYou win this round :D\n\nRock beats Scissors\n\n" + (playerName) + ": " + (++playerScore) + " - Computer: " + (computerScore) + " - Draw: " + (draw));   
+        
     }
     else if ((playerSelection==="paper")&&(computerSelection==="rock")){
-        alert ( "You win this round! Paper beats Rock\nScore: " + (playerName) + ": " + (++playerScore) + " - Computer: " + (computerScore) + " - Draw: " + (draw));    
+        return result = ( "Your opponent played " + (computerSelection) + "\n\nYou win this round :D\n\nPaper beats Rock\n\n" + (playerName) + ": " + (++playerScore) + " - Computer: " + (computerScore) + " - Draw: " + (draw));    
+        
     }
     else if ((playerSelection==="scissors")&&(computerSelection==="paper")){
-        alert ( "You win this round! Scissors beats Paper\nScore: " + (playerName)+ ": " + (++playerScore) + " - Computer: " + (computerScore) + " - Draw: " + (draw));    
+        return result = ( "Your opponent played " + (computerSelection) + "\n\nYou win this round :D\n\nScissors beats Paper\n\n" + (playerName)+ ": " + (++playerScore) + " - Computer: " + (computerScore) + " - Draw: " + (draw));    
+        
     }
     else if ((playerSelection==="rock")&&(computerSelection==="paper")){
-        alert ( "You lose this round! Paper beats Rock\nScore: " + (playerName)+ ": " + (playerScore) + " - Computer: " + (++computerScore) + " - Draw: " + (draw));    
+        return result = ( "Your opponent played " + (computerSelection) + "\n\nYou lose this round :c\n\nPaper beats Rock\n\n" + (playerName)+ ": " + (playerScore) + " - Computer: " + (++computerScore) + " - Draw: " + (draw));    
+        
     }
     else if ((playerSelection==="paper")&&(computerSelection==="scissors")){
-        alert ( "You lose this round! Scissors beats Paper\nScore: " + (playerName)+ ": " + (playerScore) + " - Computer: " + (++computerScore) + " - Draw: " + (draw));   
+        return result = ( "Your opponent played " + (computerSelection) + "\n\nYou lose this round :c\n\nScissors beats Paper\n\n" + (playerName)+ ": " + (playerScore) + " - Computer: " + (++computerScore) + " - Draw: " + (draw));   
+        
     }
     else if ((playerSelection==="scissors")&&(computerSelection==="rock")){
-     alert ( "You lose this round! Rock beats Scissors\nScore: " + (playerName)+ ": " + (playerScore) + " - Computer: " + (++computerScore) + " - Draw: " + (draw));  
+        return result = ( "Your opponent played " + (computerSelection) + "\n\nYou lose this round :c\n\nRock beats Scissors\n\n" + (playerName)+ ": " + (playerScore) + " - Computer: " + (++computerScore) + " - Draw: " + (draw));  
+        
     }
     else if (playerSelection===computerSelection){
-        alert ( "Draw!\nScore: " + (playerName)+ ": " + (playerScore) + " - Computer: " + (computerScore) + " - Draw: " + (++draw));  
+        return result = ( "Your opponent played " + (computerSelection) + "\n\nDraw!\n\n" + (playerName)+ ": " + (playerScore) + " - Computer: " + (computerScore) + " - Draw: " + (++draw));  
+        
     }
     else{     
     }
+    }    
+    
+
+
+
+    
+const body = document.querySelector('body')
+const container = document.createElement('div');
+
+const ui = document.createElement("div");
+ui.style.display = 'flex';
+ui.style.flexDirection = 'column';
+ui.style.justifyContent = 'center';
+ui.style.alignItems = 'center';
+
+ui.style.width= '600px';
+ui.style.height= '600px';
+ui.style.backgroundColor = 'transparent'
+ui.style.gap = '5px';
+const buttons = document.createElement('div');
+buttons.style.display = 'flex';
+buttons.style.justifyContent = 'space-evenly';
+buttons.style.alignItems = 'center';
+buttons.style.backgroundColor = 'lightgrey'
+buttons.style.width= '500px';
+buttons.style.height= '250px';
+buttons.style.borderRadius= '50px'
+buttons.style.border = '7px solid black'
+buttons.style.boxShadow = '20px 15px 8px grey';
+buttons.style.backgroundImage= 'url(rock-paper-scissors.gif)';
+buttons.style.backgroundSize= '500px 250px';
+
+
+const scoreTittle = document.createElement('div');
+scoreTittle.style.display = 'flex';
+scoreTittle.style.paddingTop = '10px'
+scoreTittle.style.width= '400px';
+scoreTittle.style.height= '150px';
+scoreTittle.style.justifyContent = 'center';
+scoreTittle.style.alignItems = 'flex-end'
+const scoreText = document.createElement('p');
+scoreText.textContent = 'Score'
+scoreText.style.fontSize = '30px';
+scoreText.style.color = 'black';
+scoreText.style.fontWeight = 'bold';
+scoreText.style.margin= '0px';
+scoreText.style.textShadow = '2px 2px grey';
+scoreText.style.fontFamily = 'Aclonica';
+
+
+
+const board = document.createElement('div');
+board.style.width= '400px';
+board.style.height= '150px';
+board.style.backgroundColor = 'lightblue'
+board.style.borderRadius= '50px'
+board.style.border = '5px solid black'
+board.style.boxShadow = '11px 10px 5px grey';
+board.style.display = 'flex'
+board.style.flexDirection = 'column';
+board.style.alignItems = 'center'
+board.style.justifyContent = 'center';
+
+const scoreLine = document.createElement('p');
+scoreLine.style.fontFamily = 'Aclonica';
+scoreLine.style.color = 'blue';
+scoreLine.style.margin = '0px';
+scoreLine.style.whiteSpace = 'pre-wrap';
+scoreLine.style.textAlign = 'center';
+
+
+
+
+
+const rock = document.createElement('button');
+rock.textContent= 'Rock';
+rock.style.fontFamily = 'Aclonica';
+rock.style.width='100px';
+rock.style.height='50px';
+rock.style.boxShadow = '8px 10px 5px grey';
+rock.style.backgroundColor= '#87CEFA'
+rock.style.borderRadius= '50px'
+rock.addEventListener('click', function(e){    
+    playerSelection='rock';
+    console.log(playerSelection);
+    playRound(); 
+    scoreLine.textContent = result;
+    if (playerScore===5) {
+        scoreLine.textContent = `The winner is ${playerName}!`;
+        playerScore = 0;
+        computerScore = 0;
+        draw = 0;
+    }    
+    else if (computerScore===5) {
+        scoreLine.textContent = `The winner is the computer!`;
+        playerScore = 0;
+        computerScore = 0;
+        draw = 0;
     }
-    const result = play();
-    return result;
- }
+    
+}) ;
 
-function game(){
-for (let i = 0; i < 5; i++) {
-    console.log (playRound()); 
-   } 
+const paper = document.createElement('button');
+paper.textContent = 'Paper';
+paper.style.width='100px';
+paper.style.fontFamily = 'Aclonica';
+paper.style.height='50px';
+paper.style.backgroundColor= '#87CEFA'
+paper.style.boxShadow = '8px 10px 5px grey';
+paper.style.borderRadius= '50px'
+paper.addEventListener('click', function(e){    
+    playerSelection='paper';
+    console.log(playerSelection);
+    playRound();
+    scoreLine.textContent = result;  
+    if (playerScore===5) {
+        scoreLine.textContent = `The winner is ${playerName}!`;
+        playerScore = 0;
+        computerScore = 0;
+        draw = 0;
+    }    
+    else if (computerScore===5) {
+        scoreLine.textContent = `The winner is the computer!`;
+        playerScore = 0;
+        computerScore = 0;
+        draw = 0;
+    }
+    
+}) ;
+    
 
-if (parseInt(playerScore) > parseInt(computerScore)){
-    alert(playerName + " is the winner of the game!");                
-}
-else if (parseInt(playerScore) < parseInt(computerScore)){
-    alert ("Computer is the winner of the game!");
-}
-else {
-    alert ("Draw! Nobody wins :c")
-}
-}
-console.log (game());
+
+const scissors = document.createElement('button');
+scissors.textContent = 'Scissors';
+scissors.style.width='100px';
+scissors.style.fontFamily = 'Aclonica';
+scissors.style.height='50px';
+scissors.style.boxShadow = '8px 10px 5px grey';
+scissors.style.backgroundColor= '#87CEFA'
+scissors.style.borderRadius= '50px'
+scissors.addEventListener('click', function(e){    
+    playerSelection='scissors';
+    console.log(playerSelection);
+    playRound();   
+    scoreLine.textContent = result;
+    if (playerScore===5) {
+        scoreLine.textContent = `The winner is ${playerName}!`;
+        playerScore = 0;
+        computerScore = 0;
+        draw = 0;
+    }
+    else if (computerScore===5) {
+        scoreLine.textContent = `The winner is the computer!`;
+        playerScore = 0;
+        computerScore = 0;
+        draw = 0;
+    }
+    
+}) ;
+
+body.appendChild(container);
+container.appendChild(ui);
+buttons.appendChild(rock);
+buttons.appendChild(paper);
+buttons.appendChild(scissors);
+ui.appendChild(buttons);
+ui.appendChild(scoreTittle);
+scoreTittle.appendChild(scoreText);
+ui.appendChild(board);
+board.appendChild(scoreLine);
+
+
+
+//function game(){
+//for (let i = 0; i < 5; i++) {
+//    console.log (playRound()); 
+//   } 
+//
+//if (parseInt(playerScore) > parseInt(computerScore)){
+//    alert(playerName + " is the winner of the game!");                
+//}
+//else if (parseInt(playerScore) < parseInt(computerScore)){
+//    alert ("Computer is the winner of the game!");
+//}
+//else {
+//   alert ("Draw! Nobody wins :c")
+//}
+//}
+//console.log (game());
